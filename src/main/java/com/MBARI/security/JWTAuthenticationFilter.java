44aab3774941375176f7866f8b42ee59a36c66a7
Yuki Okamoto <yokamoto@csumb.edu>
2023-11-10 17:55:19 -0800

    fix: cookie filter when login and logout

--- src/main/java/com/MBARI/security/JWTAuthenticationFilter.java
+++ src/main/java/com/MBARI/security/JWTAuthenticationFilter.java
@@ -1,3 +1,56 @@
+//package com.MBARI.security;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+//import org.springframework.util.StringUtils;
+//import org.springframework.web.filter.OncePerRequestFilter;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+///**
+// * JWT token verification
+// */
+//public class JWTAuthenticationFilter extends OncePerRequestFilter {
+//
+//    @Autowired
+//    private JWTGenerator tokenGenerator;
+//    @Autowired
+//    private CustomUserDetailsService customUserDetailsService;
+//
+//    @Override
+//    protected void doFilterInternal(HttpServletRequest request,
+//                                    HttpServletResponse response,
+//                                    FilterChain filterChain) throws ServletException, IOException {
+//        String token = getJWTFromRequest(request);
+//        if(StringUtils.hasText(token) && tokenGenerator.validateToken(token)) {
+//            String username = tokenGenerator.getUsernameFromJWT(token);
+//
+//            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+//            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null,
+//                    userDetails.getAuthorities());
+//            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+//            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+//        }
+//        filterChain.doFilter(request, response);
+//    }
+//
+//    private String getJWTFromRequest(HttpServletRequest request) {
+//        String bearerToken = request.getHeader("Authorization");
+//        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+//            return bearerToken.substring(7, bearerToken.length());
+//        }
+//        return null;
+//    }
+//}
+//
+
 package com.MBARI.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +63,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -28,7 +82,13 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String token = getJWTFromRequest(request);
+        String path = request.getRequestURI();
+        if (("/api/v1.1/auth/login".equals(path) || "/api/v1.1/auth/logout".equals(path)) && "POST".equals(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = getJWTFromCookies(request);
         if(StringUtils.hasText(token) && tokenGenerator.validateToken(token)) {
             String username = tokenGenerator.getUsernameFromJWT(token);
 
@@ -41,12 +101,15 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String getJWTFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if(StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7, bearerToken.length());
+    private String getJWTFromCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("accessToken".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
         }
         return null;
     }
 }
-
